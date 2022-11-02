c56a1b926b18d28d8f80b66a18b94e34f103ce7b
rfasich24 <pratama.fasih24@gmail.com>
2022-11-02 12:12:23 +0700

    add rekap 50%

--- src/App.js
+++ src/App.js
@@ -105,6 +105,7 @@ const App = () => {
             <>
               <NavbarAdmin />
               <TableRecap/>
+              <Footer/>
               {/* <DashboardAdmin /> */}
             </>
           }
